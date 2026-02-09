07818f5c48ddb51965f48dd8ccdf9fc41e47650b
ColorlessSaber <nuns-pastes-0s@icloud.com>
2026-02-09 15:34:34 -0800

    loaded information to screen
    Unloaded the data from the music_folder_info object and displayed it on the interface.

--- src/view.py
+++ src/view.py
@@ -7,19 +7,12 @@ from .pyside_table_object import GenericTable
 
 class PlaylistTable(GenericTable):
     """Table for showing what song(s) are in the playlist"""
-    def insert_file(self, position, rows, row_data, parent=qtc.QModelIndex()) -> None:
-        """Insert a new row into the table"""
+    def insert_rows(self, position, rows, data, parent=qtc.QModelIndex()) -> None:
+        """Insert rows into the table"""
         self.beginInsertRows(parent, position, position + rows - 1)
-        for _ in range(rows):
-            self._data.insert(position, row_data)
+        for item in data:
+            self._data.insert(position, [item]) # the PySide table needs a list of what to fill the row(s), hence the []
         self.endInsertRows()
-
-    def remove_file(self, position, rows, parent=qtc.QModelIndex()) -> None:
-        """Remove a single row from the table"""
-        self.beginRemoveRows(parent, position, position + rows - 1)
-        for _ in range(rows):
-            del(self._data[position])
-        self.endRemoveRows()
 
     def extract_data(self) -> list[Any]:
         """
@@ -29,6 +22,12 @@ class PlaylistTable(GenericTable):
 
 class MusicFolderTable(GenericTable):
     """Table for showing what folders/song files are in the folder"""
+    def insert_rows(self, position, rows, data, parent=qtc.QModelIndex()) -> None:
+        """Insert rows into the table"""
+        self.beginInsertRows(parent, position, position + rows - 1)
+        for item in data:
+            self._data.insert(position, [item]) # the PySide table needs a list of what to fill the row(s), hence the []
+        self.endInsertRows()
 
 class View(qtw.QWidget):
     """The front-end of the program"""
@@ -74,6 +73,7 @@ class View(qtw.QWidget):
                 font-weight: bold;
             }
         """)
+        self.playlist_section_group.setEnabled(False)
 
         # Songs in playlist view section
         lbl_songs_in_playlist = qtw.QLabel("Songs in Playlist", self)
@@ -90,6 +90,7 @@ class View(qtw.QWidget):
             column_names=['Song'],
         )
         self.table_songs_in_playlist_view.setModel(self.table_songs_in_playlist_model)
+        self.table_songs_in_playlist_view.setEnabled(False)
 
         layout_songs_in_playlist = qtw.QVBoxLayout()
         layout_songs_in_playlist.addWidget(lbl_songs_in_playlist)
@@ -110,6 +111,7 @@ class View(qtw.QWidget):
             column_names=['']
         )
         self.table_music_folder_view.setModel(self.table_music_folder_model)
+        self.table_music_folder_view.setEnabled(False)
 
         layout_music_folder = qtw.QVBoxLayout()
         layout_music_folder.addWidget(lbl_music_folder)
@@ -118,10 +120,13 @@ class View(qtw.QWidget):
         # Buttons and progress bar section
         self.btn_save_button = qtw.QPushButton("Save", self)
         self.btn_save_button.clicked.connect(self.save_playlist)
+        self.btn_save_button.setEnabled(False)
         self.btn_delete_button = qtw.QPushButton("Delete", self)
         self.btn_delete_button.clicked.connect(self.delete_playlist)
+        self.btn_delete_button.setEnabled(False)
         self.btn_cancel_button = qtw.QPushButton("Cancel", self)
         self.btn_cancel_button.clicked.connect(self.cancel)
+        self.btn_cancel_button.setEnabled(False)
         self.progress_bar = qtw.QProgressBar(self)
         self.progress_bar.setMinimum(0)
         self.progress_bar.setMaximum(100)
@@ -187,6 +192,8 @@ class View(qtw.QWidget):
         )
 
         if directory:
+            self._enable_widgets(False)
+            self.le_walkman_music_folder.setText(directory)
             self.signal_initiate_scan_of_music_folder.emit(directory)
 
     @qtc.Slot()
@@ -217,7 +224,26 @@ class View(qtw.QWidget):
         :param music_folder_info: An object that holds information about the Walkman MUSIC folder.
         :return:
         """
-        print(music_folder_info) #TODO add in code to update the screen information
+        # clear out old information
+        self.cb_playlist_selection.clear()
+        self.cb_playlist_selection.addItem('New Playlist', 'new_playlist')
+
+        self.table_music_folder_model.removeRows(
+            position=0,
+            rows=self.table_music_folder_model.rowCount()
+        )
+
+        # add in new information
+        for playlist in music_folder_info['playlists']:
+            self.cb_playlist_selection.addItem(playlist)
+
+        self.table_music_folder_model.insert_rows(
+            position=self.table_music_folder_model.rowCount(),
+            rows=len(music_folder_info['music_folders']),
+            data=sorted(music_folder_info['music_folders'], reverse=True) # puts the folders in A-Z, top-to-bottom order
+        )
+
+        self._enable_widgets(True)
 
     @qtc.Slot(int)
     def update_progress_bar(self, progress_value: int) -> None:
@@ -255,3 +281,21 @@ class View(qtw.QWidget):
 
         if response == qtw.QMessageBox.StandardButton.Ok:
             self.reset_progress_bar()
+
+# *** Method(s) that are private ***
+
+    def _enable_widgets(self, enable: bool) -> None:
+        """
+        Enables / disables widgets depending on the value of `enable`.
+
+        :param enable: Sets the enable / disable status of widget.
+        :return:
+        """
+        self.le_walkman_music_folder.setEnabled(enable)
+        self.btn_select_walkman_music_folder.setEnabled(enable)
+        self.playlist_section_group.setEnabled(enable)
+        self.table_songs_in_playlist_view.setEnabled(enable)
+        self.table_music_folder_view.setEnabled(enable)
+        self.btn_save_button.setEnabled(enable)
+        self.btn_delete_button.setEnabled(enable)
+        self.btn_cancel_button.setEnabled(enable)
